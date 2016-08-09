8a72266d1415f3aefea6ce4cdaecbee4ac8668cc
mwieser <manuel.wieser@karriere.at>
2016-08-09 14:47:37 +0200

    PSR-2 formatting

--- src/Compilers/BladeCompiler.php
+++ src/Compilers/BladeCompiler.php
@@ -3,18 +3,18 @@
 use Illuminate\View\Compilers\BladeCompiler as BaseBladeCompiler;
 use Illuminate\View\Compilers\CompilerInterface;
 
-class BladeCompiler extends BaseBladeCompiler implements CompilerInterface {
+class BladeCompiler extends BaseBladeCompiler implements CompilerInterface
+{
 
     /**
      * Compile the include statements into valid PHP.
      *
-     * @param  string  $expression
+     * @param  string $expression
      * @return string
      */
     protected function compileRaw($expression)
     {
-        if (starts_with($expression, '('))
-        {
+        if (starts_with($expression, '(')) {
             $expression = substr($expression, 1, -1);
         }
 
